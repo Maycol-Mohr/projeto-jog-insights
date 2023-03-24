f16fb132c991719bde5c2afd60bea206098a9d04
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-03-24 20:17:51 +0100

    tentando exercicio 2 com ajuda

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -14,21 +14,10 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    unique_jobs_type = set()
+    for job in read(path):
+        unique_jobs_type.add(job["job_type"])
+    return list(unique_jobs_type)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
