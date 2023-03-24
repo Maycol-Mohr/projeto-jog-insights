de3903ed8506b06488eb2fa8ccba53c951507724
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-03-24 23:58:25 +0100

    tentando exercicio 8 com ajuda

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -19,29 +19,25 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    if (
+        not isinstance(salary, (int, str))
+        or 'min_salary' not in job
+        or 'max_salary' not in job
+        or not str(job['min_salary']).isdigit()
+        or not str(job['max_salary']).isdigit()
+        or not int(job["min_salary"]) and int(job['min_salary']) != 0
+        or not int(job["max_salary"])
+        or int(job["min_salary"]) > int(job["max_salary"])
+        or not salary and int(salary) != 0
+    ):
+        raise ValueError(
+            "Error in max-salary or min-salary or salary, "
+            "incorrect values or ranges"
+        )
+    if int(job['min_salary']) <= int(salary) <= int(job['max_salary']):
+        return True
+    else:
+        return False
 
 
 def filter_by_salary_range(
