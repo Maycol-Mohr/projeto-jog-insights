7149bdc6b3368ace49f01f6adf498e2d005192ff
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-03-25 01:38:30 +0100

    tentando exercicio 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+# from unittest.mock import mock_open, patch
+# import pytest
 
 
 def test_counter():
-    pass
+    with patch("builtins.open", mock_open(read_data="file")):
+        assert count_ocurrences('path', 'FILE') == 1
