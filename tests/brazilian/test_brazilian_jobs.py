d689655e65127b356c469f85eb0e7cdab24038da
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-03-25 19:39:49 +0100

    tentando exercicio 12 com ajuda

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,28 @@
 # from src.pre_built.brazilian_jobs import read_brazilian_file
+# from src.insights.jobs import read
+# from unittest.mock import mock_open, patch
+# import pytest
 
 
-def test_brazilian_jobs():
-    pass
+# @pytest.fixture
+# def english_type():
+#     return {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+
+
+# # @pytest.fixture
+# # def portuguese_type():
+# #     return {"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"}
+
+
+# def test_brazilian_jobs():
+#     with patch("builtins.open", mock_open(read_data='read_pt_file')):
+#         read_file = read(patch)
+#         # file_path =  read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+
+#     dict_jobs = read_file
+#     for job in dict_jobs:
+#         job["title"] = job.pop("titulo")
+#         job["salary"] = job.pop("salario")
+#         job["type"] = job.pop("tipo")
+
+#     assert read_brazilian_file(dict_jobs) == english_type
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,43 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+# from unittest.mock import mock_open, patch
+# from unittest.mock import mock_open, patch
+# import pytest
+
+#  with patch("builtins.open", mock_open(read_data="file")):
+#         assert count_ocurrences('path', 'FILE') == 1
+
+jobs_list = [
+    {"min_salary": 5000, "max_salary": 45000, "date_posted": "2000-05-30"},
+    {"min_salary": 8000, "max_salary": 35000, "date_posted": "2020-05-30"},
+    {"min_salary": 6000, "max_salary": 25000, "date_posted": "2010-05-30"},
+]
+
+jobs_min_salary = [
+    {"min_salary": 5000, "max_salary": 45000, "date_posted": "2000-05-30"},
+    {"min_salary": 6000, "max_salary": 25000, "date_posted": "2010-05-30"},
+    {"min_salary": 8000, "max_salary": 35000, "date_posted": "2020-05-30"},
+]
+
+jobs_max_salary = [
+    {"min_salary": 5000, "max_salary": 45000, "date_posted": "2000-05-30"},
+    {"min_salary": 8000, "max_salary": 35000, "date_posted": "2020-05-30"},
+    {"min_salary": 6000, "max_salary": 25000, "date_posted": "2010-05-30"},
+]
+
+
+jobs_date = [
+   {"min_salary": 8000, "max_salary": 35000, "date_posted": "2020-05-30"},
+   {"min_salary": 6000, "max_salary": 25000, "date_posted": "2010-05-30"},
+   {"min_salary": 5000, "max_salary": 45000, "date_posted": "2000-05-30"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs_list, 'min_salary')
+    assert jobs_list == jobs_min_salary
+
+    sort_by(jobs_list, 'max_salary')
+    assert jobs_list == jobs_max_salary
+
+    sort_by(jobs_list, 'date_posted')
+    assert jobs_list == jobs_date
