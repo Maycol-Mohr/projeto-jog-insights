9604eac0bdc2cbee2dbce3a63e13562dcd462962
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-03-26 13:49:47 +0200

    fix: exercicio 11

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,9 +1,9 @@
 from src.pre_built.counter import count_ocurrences
 from unittest.mock import mock_open, patch
-# from unittest.mock import mock_open, patch
-# import pytest
+
+mock_string = 'TesTe'
 
 
 def test_counter():
-    with patch("builtins.open", mock_open(read_data="file")):
-        assert count_ocurrences('path', 'FILE') == 1
+    with patch("builtins.open", mock_open(read_data=mock_string)):
+        assert count_ocurrences('path', 'tEStE') == 1
